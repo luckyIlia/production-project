c07e09c4872b286430d001870ad140c0f23f1c5c
Ilia Paramoshin <ilia.dev@yahoo.com>
2023-05-15 09:26:41 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+// @ts-ignore
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./AboutPage')), 1500)
+}));
