746cb856e1c226097344f913377923e359ed7744
luckyilia <ilia.dev@yahoo.com>
2023-05-21 13:30:01 +0300

    fixed eslint config fixed all files

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,26 +1,28 @@
-import { type BuildOptions } from './types/config'
-import type webpack from 'webpack'
-import { buildPlugins } from './buildPlugins'
-import { buildLoaders } from './buildLoaders'
-import { buildResolvers } from './buildResolvers'
-import { buildDevServer } from './buildDevServer'
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
-export function buildWebpackConfig (option: BuildOptions): webpack.Configuration {
-  const { paths, mode, isDev } = option
-  return {
-    mode,
-    entry: paths.entry,
-    output: {
-      filename: '[name].[contenthash].js',
-      path: paths.build,
-      clean: true
-    },
-    plugins: buildPlugins(option),
-    module: {
-      rules: buildLoaders(option)
-    },
-    resolve: buildResolvers(option),
-    devtool: isDev ? 'inline-source-map' : undefined,
-    devServer: isDev ? buildDevServer(option) : undefined
-  }
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
