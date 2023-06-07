83287ca84875d6f1220a3a3c0f2e397196a22bf5
luckyilia <ilia.dev@yahoo.com>
2023-06-08 00:55:03 +0300

    add redux toolkit

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,37 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector((state: StateSchema) => state.counter.value);
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+    return (
+        <div>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <h1>
+                value =
+                {counterValue}
+            </h1>
+            {/* eslint-disable */}
+            <Button
+                onClick={increment}
+            >
+                increment
+            </Button>
+            <Button
+                onClick={decrement}
+            >
+                decrement
+            </Button>
+        </div>
+
+    );
+};
