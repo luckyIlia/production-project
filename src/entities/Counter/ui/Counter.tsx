3a00ea9c27b694ec2ab773ef58e9bd36f36fb057
luckyilia <ilia.dev@yahoo.com>
2023-06-26 23:30:16 +0300

    fix tests

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,12 +1,14 @@
 import { Button } from 'shared/ui/Button/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { useTranslation } from 'react-i18next';
 import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
     const dispatch = useDispatch();
     const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
+
     const increment = () => {
         dispatch(counterActions.increment());
     };
@@ -14,25 +16,22 @@ export const Counter = () => {
     const decrement = () => {
         dispatch(counterActions.decrement());
     };
+
     return (
         <div>
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <h1>
-                value =
-                {counterValue}
-            </h1>
-            {/* eslint-disable */}
+            <h1 data-testid="value-title">{counterValue}</h1>
             <Button
                 onClick={increment}
+                data-testid="increment-btn"
             >
-                increment
+                {t('increment')}
             </Button>
             <Button
+                data-testid="decrement-btn"
                 onClick={decrement}
             >
-                decrement
+                {t('decrement')}
             </Button>
         </div>
-
     );
 };
