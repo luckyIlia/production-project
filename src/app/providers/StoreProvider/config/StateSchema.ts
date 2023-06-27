37ec55cbe2fd87792371a44b3af0d5c775732099
luckyilia <ilia.dev@yahoo.com>
2023-06-27 23:58:51 +0300

    add reducerManager

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -5,5 +5,9 @@ import { LoginSchema } from 'features/AuthByUsername';
 export interface StateSchema {
     counter: CounterSchema;
     user: UserSchema;
-    loginForm: LoginSchema;
+
+    // asynvc reducers
+    loginForm?: LoginSchema;
 }
+
+export type StateSchemaKey = keyof StateSchema;
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -10,11 +10,11 @@ import { getLoginState } from '../../model/selectors/getLoginState/getLoginState
 import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
-interface LoginFormProps {
+export interface LoginFormProps {
     className?: string;
 }
 
-export const LoginForm = memo(({ className }: LoginFormProps) => {
+const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
     const {
@@ -59,3 +59,5 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
         </div>
     );
 });
+
+export default LoginForm;
