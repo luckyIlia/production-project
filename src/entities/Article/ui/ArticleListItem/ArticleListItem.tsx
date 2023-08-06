7832f8116e942b34a57519216c052b030e502377
luckyilia <ilia.dev@yahoo.com>
2023-08-06 23:45:36 +0300

    add card ui

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,6 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { Text } from 'shared/ui/Text/Text';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { Card } from 'shared/ui/Card/Card';
 import cls from './ArticleListItem.module.scss';
 import { Article, ArticleView } from '../../model/types/Article';
 
@@ -18,9 +22,28 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
     } = props;
     const { t } = useTranslation();
 
+    if (view === ArticleView.BIG) {
+        return (
+            <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+                {article.title}
+            </div>
+        );
+    }
+
     return (
-        <div className={classNames(cls.ArticleListItem, {}, [className])}>
-            {article.title}
+        <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+            <Card className={cls.card}>
+                <div className={cls.imageWrapper}>
+                    <img alt={article.title} src={article.img} className={cls.img} />
+                    <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <div className={cls.infoWrapper}>
+                    <Text text={article.type.join(', ')} className={cls.types} />
+                    <Text text={String(article.views)} className={cls.views} />
+                    <Icon Svg={EyeIcon} />
+                </div>
+                <Text text={article.title} className={cls.title} />
+            </Card>
         </div>
     );
 });
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,12 +1,16 @@
+// eslint-disable max-len
+
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
-import { ArticleList } from 'entities/Article';
+import { Article, ArticleList } from 'entities/Article';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
     className?: string;
 }
+const article = {
+} as Article;
 
 const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
@@ -15,7 +19,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     return (
         // eslint-disable-next-line i18next/no-literal-string
         <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            <ArticleList articles={[]} />
+            <ArticleList articles={[article]} />
         </div>
     );
 };
