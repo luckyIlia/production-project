386cd236c278ae908d96439e08bb4245debaf583
luckyilia <ilia.dev@yahoo.com>
2023-08-23 10:29:27 +0300

    fix translations

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -12,7 +12,7 @@ export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
 
     const toggle = async () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+        await i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
     };
 
     return (
@@ -21,6 +21,7 @@ export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
             theme={ButtonTheme.CLEAR}
             onClick={toggle}
         >
+            {/* eslint-disable-next-line i18next/no-literal-string */}
             {t(short ? 'Короткий язык' : 'Язык')}
         </Button>
     );
