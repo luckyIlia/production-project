f6858e875350a1fa8adaa69810124d45283bce3a
luckyilia <ilia.dev@yahoo.com>
2023-08-28 23:56:01 +0300

    add eslint plugin, fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
+        'luckyilia-plugin',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -33,7 +34,7 @@ module.exports = {
         ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
+        'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
@@ -58,6 +59,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
+        'luckyilia-plugin/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
