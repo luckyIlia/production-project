9e176c2448ddd2b9581a8a543a5352613d8fd1f6
luckyilia <ilia.dev@yahoo.com>
2023-09-26 00:53:08 +0300

    fix screenshots

--- src/shared/ui/Popups/components/Popover/Popover.tsx
+++ src/shared/ui/Popups/components/Popover/Popover.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Popover as HPopover } from '@headlessui/react';
 import { DropdownDirection } from 'shared/types/ui';
 import { ReactNode } from 'react';
-import cls from '../../styles/components/Dropdown/Dropdown.module.scss';
 import { mapDirectionClass } from '../../styles/consts';
 import popupCls from '../../styles/popup.module.scss';
 
@@ -21,14 +20,14 @@ export function Popover(props: PopoverProps) {
 
     return (
         <HPopover
-            className={classNames(cls.Popover, {}, [className, popupCls.popup])}
+            className={classNames(popupCls.Popover, {}, [className, popupCls.popup])}
         >
             <HPopover.Button className={popupCls.trigger}>
                 {trigger}
             </HPopover.Button>
 
             <HPopover.Panel
-                className={classNames(cls.panel, {}, menuClasses)}
+                className={classNames(popupCls.panel, {}, menuClasses)}
             >
                 {children}
             </HPopover.Panel>
