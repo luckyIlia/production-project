342d15778029c31389301a8b75a77834807fbdc5
luckyilia <ilia.dev@yahoo.com>
2023-10-01 00:03:53 +0300

    remove virtualized

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import {
+    useArticleRecommendationsList,
+} from '../../api/articleRecommendationsApi';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Text, TextSize } from '@/shared/ui/Text/Text';
 import { ArticleList } from '@/entities/Article';
 import { VStack } from '@/shared/ui/Stack';
-import {
-    useArticleRecommendationsList,
-} from '../../api/articleRecommendationsApi';
 
 interface ArticleRecommendationsListProps {
     className?: string;
@@ -16,9 +16,11 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
     const { className } = props;
     const { t } = useTranslation();
     const { isLoading, data: articles, error } = useArticleRecommendationsList(3);
+
     if (isLoading || error || !articles) {
         return null;
     }
+
     return (
         <VStack gap="8" className={classNames('', {}, [className])}>
             <Text
@@ -28,7 +30,6 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
             <ArticleList
                 articles={articles}
                 target="_blank"
-                virtualized={false}
             />
         </VStack>
     );
