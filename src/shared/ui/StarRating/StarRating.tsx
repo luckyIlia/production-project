d24c9d3936c316023e936f45eb71a061ae906db8
luckyilia <ilia.dev@yahoo.com>
2023-10-07 23:48:47 +0300

    add star rating

---
+++ src/shared/ui/StarRating/StarRating.tsx
@@ -0,0 +1,56 @@
+import { memo, useState } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './StarRating.module.scss';
+import StarIcon from '@/shared/assets/icons/star.svg';
+import { Icon } from '../Icon/Icon';
+
+interface StarRatingProps {
+    className?: string;
+    onSelect?: (starsCount: number) => void;
+    size?: number;
+    selectedStars?: number;
+}
+
+const stars = [1, 2, 3, 4, 5];
+
+export const StarRating = memo((props: StarRatingProps) => {
+    const {
+        className, selectedStars = 0, onSelect, size = 30,
+    } = props;
+    const [currentStarsCount, setCurrentStarsCount] = useState(0);
+    const [isSelected, setIsSelected] = useState(Boolean(selectedStars));
+
+    const onHover = (starsCount: number) => () => {
+        if (!isSelected) {
+            setCurrentStarsCount(starsCount);
+        }
+    };
+
+    const onLeave = () => {
+        if (!isSelected) {
+            setCurrentStarsCount(0);
+        }
+    };
+
+    return (
+        <div
+            className={classNames(cls.StarRating, {}, [className])}
+        >
+            {stars.map((starNumber) => (
+                <Icon
+                    className={classNames(
+                        cls.starIcon,
+                        {},
+                        [currentStarsCount >= starNumber ? cls.hovered : cls.normal],
+                    )}
+                    Svg={StarIcon}
+                    key={starNumber}
+                    width={size}
+                    height={size}
+                    onMouseLeave={onLeave}
+                    onMouseEnter={onHover(starNumber)}
+                />
+            ))}
+        </div>
+    );
+});
