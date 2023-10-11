72bdeaecc9668829de00878d12e603ffb0b20438
luckyilia <ilia.dev@yahoo.com>
2023-10-11 23:50:28 +0300

    add article rating async

--- src/features/articleRating/api/articleRatingApi.ts
+++ src/features/articleRating/api/articleRatingApi.ts
@@ -1,9 +1,21 @@
 import { rtkApi } from '@/shared/api/rtkApi';
 import { Rating } from '@/entities/Rating';
 
+interface GetArticleRatingArg {
+    userId: string;
+    articleId: string;
+}
+
+interface RateArticleArg {
+    userId: string;
+    articleId: string;
+    rate: number;
+    feedback?: string;
+}
+
 const articleRatingApi = rtkApi.injectEndpoints({
     endpoints: (build) => ({
-        getArticleRating: build.query<Rating[], {userId: string, articleId: string}>({
+        getArticleRating: build.query<Rating[], GetArticleRatingArg>({
             query: ({ userId, articleId }) => ({
                 url: '/article-ratings',
                 params: {
@@ -12,7 +24,15 @@ const articleRatingApi = rtkApi.injectEndpoints({
                 },
             }),
         }),
+        rateArticle: build.mutation<void, RateArticleArg>({
+            query: (arg) => ({
+                url: '/article-ratings',
+                method: 'POST',
+                body: arg,
+            }),
+        }),
     }),
 });
 
 export const useGetArticleRating = articleRatingApi.useGetArticleRatingQuery;
+export const useRateArticle = articleRatingApi.useRateArticleMutation;
--- src/features/articleRating/ui/ArticleRating/ArticleRating.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.tsx
@@ -1,24 +1,48 @@
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { RatingCard } from '@/entities/Rating';
-import { useGetArticleRating } from '../../api/articleRatingApi';
+import { useGetArticleRating, useRateArticle } from '../../api/articleRatingApi';
 import { getUserAuthData } from '@/entities/User';
 import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
 
-interface ArticleRatingProps {
+export interface ArticleRatingProps {
     className?: string;
     articleId: string;
 }
 
-export const ArticleRating = memo((props: ArticleRatingProps) => {
+const ArticleRating = memo((props: ArticleRatingProps) => {
     const { className, articleId } = props;
     const { t } = useTranslation();
     const userData = useSelector(getUserAuthData);
+
     const { data, isLoading } = useGetArticleRating({
         articleId,
         userId: userData?.id ?? '',
     });
+    const [rateArticleMutation] = useRateArticle();
+
+    const handleRateArticle = useCallback((starsCount: number, feedback?: string) => {
+        try {
+            rateArticleMutation({
+                userId: userData?.id ?? '',
+                articleId,
+                rate: starsCount,
+                feedback,
+            });
+        } catch (e) {
+            // handle error
+            console.log(e);
+        }
+    }, [articleId, rateArticleMutation, userData?.id]);
+
+    const onAccept = useCallback((starsCount: number, feedback?: string) => {
+        handleRateArticle(starsCount, feedback);
+    }, [handleRateArticle]);
+
+    const onCancel = useCallback((starsCount: number) => {
+        handleRateArticle(starsCount);
+    }, [handleRateArticle]);
 
     if (isLoading) {
         return <Skeleton width="100%" height={120} />;
@@ -28,6 +52,8 @@ export const ArticleRating = memo((props: ArticleRatingProps) => {
 
     return (
         <RatingCard
+            onCancel={onCancel}
+            onAccept={onAccept}
             rate={rating?.rate}
             className={className}
             title={t('Оцените статью')}
@@ -36,3 +62,5 @@ export const ArticleRating = memo((props: ArticleRatingProps) => {
         />
     );
 });
+
+export default ArticleRating;
