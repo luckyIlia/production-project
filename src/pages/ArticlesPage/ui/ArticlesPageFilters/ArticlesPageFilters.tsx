f70337acb6d4a00b1da3535a5e471527da7f7454
luckyilia <ilia.dev@yahoo.com>
2023-10-16 23:41:23 +0300

    fix absolute imports

--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -7,6 +7,7 @@ import {
     ArticleTypeTabs,
     ArticleView,
     ArticleViewSelector,
+    ArticleType,
 } from '@/entities/Article';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -14,7 +15,6 @@ import { Card } from '@/shared/ui/Card/Card';
 import { Input } from '@/shared/ui/Input/Input';
 import { SortOrder } from '@/shared/types';
 import { useDebounce } from '@/shared/lib/hooks/useDebounce/useDebounce';
-import { ArticleType } from '@/entities/Article/model/consts/articleConsts';
 import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 import cls from './ArticlesPageFilters.module.scss';
 import {
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,14 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+// TODO
+// eslint-disable-next-line luckyilia-plugin/public-api-imports
 import { loginReducer } from '@/features/AuthByUsername/model/slice/loginSlice';
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+// eslint-disable-next-line luckyilia-plugin/public-api-imports
 import { articleDetailsReducer } from '@/entities/Article/model/slice/articleDetailsSlice';
+// eslint-disable-next-line luckyilia-plugin/public-api-imports
 import { addCommentFormReducer } from '@/features/addCommentForm/model/slices/addCommentFormSlice';
+// eslint-disable-next-line luckyilia-plugin/public-api-imports
 import { profileReducer } from '@/features/editableProfileCard/model/slice/profileSlice';
 
 const defaultAsyncReducers: ReducersList = {
