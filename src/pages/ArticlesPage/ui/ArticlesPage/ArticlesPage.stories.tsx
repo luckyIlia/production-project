887a639ceea535ade0e38d4deeb0c99afc3dd7aa
luckyilia <ilia.dev@yahoo.com>
2023-11-21 23:26:41 +0300

    fix screens

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
 
-import ArticlesPage from "./ArticlesPage";
+import ArticlesPage from './ArticlesPage';
 
 export default {
-  title: "pages/ArticlesPage/ArticlesPage",
-  component: ArticlesPage,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
-} as ComponentMeta<typeof ArticlesPage>;
+    title: 'pages/ArticlesPage/ArticlesPage',
+    component: ArticlesPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof ArticlesPage>;
 
-const Template: ComponentStory<typeof ArticlesPage> = (args) => (
-  <ArticlesPage {...args} />
+const Template: StoryFn<typeof ArticlesPage> = (args) => (
+    <ArticlesPage {...args} />
 );
 
 export const Normal = Template.bind({});
