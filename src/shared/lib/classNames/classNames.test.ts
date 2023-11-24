4cdb553d3a508b4bbe895e8229ec09f886021343
luckyilia <ilia.dev@yahoo.com>
2023-11-25 00:10:57 +0300

    add unit tests, fix screens

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -39,4 +39,43 @@ describe("classNames", () => {
       ]),
     ).toBe(expected);
   });
+  test("with no parameters", () => {
+    expect(classNames).toBe("");
+  });
+  test("with a single mod and additional classes", () => {
+    const expected = "someClass class1 class2 hovered";
+    expect(
+        classNames("someClass", { hovered: true }, ["class1", "class2"]),
+    ).toBe(expected);
+  });
+  test("with falsy values in mods", () => {
+    const expected = "someClass class1 class2";
+    expect(
+        // @ts-ignore
+        classNames("someClass", { hovered: false, scrollable: null }, [
+          "class1",
+          "class2",
+        ]),
+    ).toBe(expected);
+  });
+  test("with multiple classes for a mod", () => {
+    const expected = "someClass class1 class2 active";
+    expect(
+        classNames("someClass", { active: true }, ["class1", "class2"]),
+    ).toBe(expected);
+  });
+  test("with an empty mod object", () => {
+    const expected = "someClass class1 class2";
+    expect(classNames("someClass", {}, ["class1", "class2"])).toBe(expected);
+  });
+  test("with multiple mods and additional classes", () => {
+    const expected = "someClass class1 class2 hovered active";
+    expect(
+        classNames("someClass", { hovered: true, active: true }, [
+          "class1",
+          "class2",
+        ]),
+    ).toBe(expected);
+  });
+
 });
