f95074bbf2e2113e77172cbc37779c71e9100ad9
luckyilia <ilia.dev@yahoo.com>
2023-11-27 22:28:03 +0300

    add unit tests

--- src/features/editableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
+++ src/features/editableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
@@ -1,7 +1,7 @@
 import { StateSchema } from "@/app/providers/StoreProvider";
-import { Country } from "@/entities/Country";
 import { Currency } from "@/entities/Currency";
 import { getProfileData } from "./getProfileData";
+import { Country } from '@/entities/Country';
 
 describe("getProfileData.test", () => {
   test("should return error", () => {
@@ -25,4 +25,38 @@ describe("getProfileData.test", () => {
     const state: DeepPartial<StateSchema> = {};
     expect(getProfileData(state as StateSchema)).toEqual(undefined);
   });
+  test("should return an empty object when profile data is an empty object", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        data: {},
+      },
+    };
+    expect(getProfileData(state as StateSchema)).toEqual({});
+  });
+  test("should return undefined when profile is undefined", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: undefined,
+    };
+    expect(getProfileData(state as StateSchema)).toEqual(undefined);
+  });
+  test("should return profile data with unexpected fields", () => {
+    const dataWithExtraFields = {
+      username: "admin",
+      age: 22,
+      country: Country.Armenia,
+      lastname: "john doe",
+      first: "asd",
+      city: "asf",
+      currency: Currency.USD,
+      extraField: "additional value",
+    };
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        data: dataWithExtraFields,
+      },
+    };
+    expect(getProfileData(state as StateSchema)).toEqual(dataWithExtraFields);
+  });
+
+
 });
