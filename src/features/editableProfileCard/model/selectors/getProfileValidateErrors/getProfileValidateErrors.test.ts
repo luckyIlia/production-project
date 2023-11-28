e0054dbfb3f6f24d66bc50646fb26054e497376e
luckyilia <ilia.dev@yahoo.com>
2023-11-28 22:14:53 +0300

    add unit tests

--- src/features/editableProfileCard/model/selectors/getProfileValidateErrors/getProfileValidateErrors.test.ts
+++ src/features/editableProfileCard/model/selectors/getProfileValidateErrors/getProfileValidateErrors.test.ts
@@ -21,4 +21,44 @@ describe("getProfileValidateErrors.test", () => {
     const state: DeepPartial<StateSchema> = {};
     expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined);
   });
+  describe("getProfileValidateErrors.test", () => {
+    test("should work with undefined profile", () => {
+      const state: DeepPartial<StateSchema> = {}; // No profile property
+      expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined);
+    });
+
+    test("should work with null validateErrors in profile", () => {
+      const state: DeepPartial<StateSchema> = {
+        profile: {
+          // @ts-ignore
+          validateErrors: null,
+        },
+      };
+      expect(getProfileValidateErrors(state as StateSchema)).toEqual(null);
+    });
+
+    test("should work with empty validateErrors in profile", () => {
+      const state: DeepPartial<StateSchema> = {
+        profile: {
+          validateErrors: [],
+        },
+      };
+      expect(getProfileValidateErrors(state as StateSchema)).toEqual([]);
+    });
+
+    test("should work with missing profile property", () => {
+      const state: DeepPartial<StateSchema> = {
+        // No profile property in the state
+      };
+      expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined);
+    });
+
+    test("should work with empty profile", () => {
+      const state: DeepPartial<StateSchema> = {
+        profile: {}, // Empty profile object
+      };
+      expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined);
+    });
+  });
+
 });
