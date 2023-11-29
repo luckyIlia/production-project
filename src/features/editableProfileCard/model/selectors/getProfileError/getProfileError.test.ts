5d5c09bfe86191f3beff4720c922c6c75f7cf7f3
luckyilia <ilia.dev@yahoo.com>
2023-11-29 22:57:45 +0300

    add unit tests

--- src/features/editableProfileCard/model/selectors/getProfileError/getProfileError.test.ts
+++ src/features/editableProfileCard/model/selectors/getProfileError/getProfileError.test.ts
@@ -14,4 +14,65 @@ describe("getProfileError.test", () => {
     const state: DeepPartial<StateSchema> = {};
     expect(getProfileError(state as StateSchema)).toEqual(undefined);
   });
+  test("should return undefined when profile is null", () => {
+    const state: DeepPartial<StateSchema> = {
+      // @ts-ignore
+      profile: null,
+    };
+    expect(getProfileError(state as StateSchema)).toEqual(undefined);
+  });
+
+  test("should return undefined when profile is undefined", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: undefined,
+    };
+    expect(getProfileError(state as StateSchema)).toEqual(undefined);
+  });
+  test("should return error string", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        error: "Some error message",
+      },
+    };
+    expect(getProfileError(state as StateSchema)).toEqual("Some error message");
+  });
+
+  test("should return null as error", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        // @ts-ignore
+        error: null,
+      },
+    };
+    expect(getProfileError(state as StateSchema)).toEqual(null);
+  });
+
+  test("should return 0 as error", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        // @ts-ignore
+        error: 0,
+      },
+    };
+    expect(getProfileError(state as StateSchema)).toEqual(0);
+  });
+
+  test("should handle missing profile property", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getProfileError(state as StateSchema)).toEqual(undefined);
+  });
+
+  test("should handle invalid profile structure", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        // Missing 'error' property
+        // or unexpected structure
+        // Add an unexpected property to profile to simulate an invalid structure
+        // For example:
+        // @ts-ignore
+        someUnexpectedProperty: "unexpected",
+      },
+    };
+    expect(getProfileError(state as StateSchema)).toEqual(undefined);
+  });
 });
