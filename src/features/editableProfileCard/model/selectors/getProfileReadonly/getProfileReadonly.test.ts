030d4992e30351519c2f8809b28705bc965ea9a8
luckyilia <ilia.dev@yahoo.com>
2023-11-30 23:19:12 +0300

    add unit tests

--- src/features/editableProfileCard/model/selectors/getProfileReadonly/getProfileReadonly.test.ts
+++ src/features/editableProfileCard/model/selectors/getProfileReadonly/getProfileReadonly.test.ts
@@ -14,4 +14,37 @@ describe("getProfileReadonly.test", () => {
     const state: DeepPartial<StateSchema> = {};
     expect(getProfileReadonly(state as StateSchema)).toEqual(undefined);
   });
+  test("should work with null profile", () => {
+    const state: DeepPartial<StateSchema> = {
+      // @ts-ignore
+      profile: null,
+    };
+    expect(getProfileReadonly(state as StateSchema)).toEqual(undefined);
+  });
+  test("should work when readonly property is missing", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        // missing readonly property
+      },
+    };
+    expect(getProfileReadonly(state as StateSchema)).toEqual(undefined);
+  });
+  test("should work with readonly set to false", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        readonly: false,
+      },
+    };
+    expect(getProfileReadonly(state as StateSchema)).toEqual(false);
+  });
+
+  test("should work with readonly set to a non-boolean value", () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        // @ts-ignore
+        readonly: "someValue", // for example, a string instead of a boolean
+      },
+    };
+    expect(getProfileReadonly(state as StateSchema)).toEqual("someValue");
+  });
 });
