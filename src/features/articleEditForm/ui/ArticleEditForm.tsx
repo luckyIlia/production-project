744f8cd6b0b1021e641ba76c6a7570ca20d437ea
luckyilia <ilia.dev@yahoo.com>
2023-12-04 22:42:46 +0300

    add article edit form

--- src/features/articleEditForm/ui/ArticleEditForm.tsx
+++ src/features/articleEditForm/ui/ArticleEditForm.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './ArticleEditForm.module.scss';
 
@@ -11,12 +11,49 @@ export const ArticleEditForm = memo((props: ArticleEditFormProps) => {
     const { className } = props;
     const { t } = useTranslation();
 
+    const [articleData, setArticleData] = useState({
+        title: '',
+        content: '',
+    });
+
+    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setArticleData({
+            ...articleData,
+            title: event.target.value,
+        });
+    };
+
+    const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setArticleData({
+            ...articleData,
+            content: event.target.value,
+        });
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
     return (
-        // eslint-disable-next-line i18next/no-literal-string
-        <div className={classNames(cls.ArticleEditForm, {}, [className])}>
-            ARTICLE EDIT FORM // TODO
-        </div>
+        <form onSubmit={handleSubmit} className={classNames(cls.ArticleEditForm, {}, [className])}>
+            <div>
+                <label htmlFor="articleTitle">{t('Title')}</label>
+                <input
+                    type="text"
+                    id="articleTitle"
+                    value={articleData.title}
+                    onChange={handleTitleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="articleContent">{t('Content')}</label>
+                <textarea
+                    id="articleContent"
+                    value={articleData.content}
+                    onChange={handleContentChange}
+                />
+            </div>
+            <button type="submit">{t('Submit')}</button>
+        </form>
     );
 });
-
-
