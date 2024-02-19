295204efd37f114b377d1bb5badcb8e5144b6d57
luckyilia <ilia.dev@yahoo.com>
2024-02-19 23:49:48 +0300

    add toggle features

--- src/shared/lib/features/toggleFeatures.ts
+++ src/shared/lib/features/toggleFeatures.ts
@@ -1,5 +1,5 @@
 import { FeatureFlags } from '@/shared/types/featureFlags';
-import { getFeatureFlags } from './setGetFeatures';
+import { getFeatureFlag } from './setGetFeatures';
 
 interface ToggleFeaturesOptions<T> {
     name: keyof FeatureFlags;
@@ -8,7 +8,7 @@ interface ToggleFeaturesOptions<T> {
 }
 
 export function toggleFeatures<T>({off, on, name}: ToggleFeaturesOptions<T>): T {
-    if(getFeatureFlags(name)) {
+    if(getFeatureFlag(name)) {
         return on();
     }
     return off();
