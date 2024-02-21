0b029de207a3b042539acc0141a87b3e4d2b0b94
luckyilia <ilia.dev@yahoo.com>
2024-02-21 23:27:25 +0300

    add redesigned fonts and colors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
-import React, { Suspense, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "@/widgets/Navbar";
-import { Sidebar } from "@/widgets/Sidebar";
-import { getUserInited , initAuthData } from '@/entities/User';
-import { useTheme } from "@/shared/lib/hooks/useTheme/useTheme";
+import React, { Suspense, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { getUserInited, initAuthData } from '@/entities/User';
+import { AppRouter } from './providers/router';
+import { Navbar } from '@/widgets/Navbar';
+import { Sidebar } from '@/widgets/Sidebar';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { PageLoader } from '@/widgets/PageLoader';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 function App() {
   const { theme } = useTheme();
@@ -18,20 +19,36 @@ function App() {
     dispatch(initAuthData());
   }, [dispatch]);
 
-  if(!inited) {
-    return <PageLoader />
+  if (!inited) {
+    return <PageLoader />;
   }
 
   return (
-    <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          {inited && <AppRouter />}
-        </div>
-      </Suspense>
-    </div>
+      <ToggleFeatures
+          feature="isAppRedesigned"
+          off={
+            <div className={classNames('app', {}, [theme])}>
+              <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                  <Sidebar />
+                  <AppRouter />
+                </div>
+              </Suspense>
+            </div>
+          }
+          on={
+            <div className={classNames('app_redesigned', {}, [theme])}>
+              <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                  <Sidebar />
+                  <AppRouter />
+                </div>
+              </Suspense>
+            </div>
+          }
+      />
   );
 }
 
