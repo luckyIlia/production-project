fe3e8d7db3c28f3b3d3a6fdcc587f5d93adc83b1
luckyilia <ilia.dev@yahoo.com>
2024-02-25 22:06:10 +0300

    add navbar redesign

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,11 +1,11 @@
-import { memo } from "react";
-import { useSelector } from "react-redux";
-import { useTranslation } from "react-i18next";
-import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import { getUserAuthData } from "@/entities/User";
-import { SidebarItemType } from "../../model/types/sidebar";
-import cls from "./SidebarItem.module.scss";
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { getUserAuthData } from '@/entities/User';
+import { SidebarItemType } from '../../model/types/sidebar';
+import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
   item: SidebarItemType;
@@ -21,13 +21,13 @@ export const SidebarItem = memo(({ item, collapsed }: SidebarItemProps) => {
   }
 
   return (
-    <AppLink
-      theme={AppLinkTheme.SECONDARY}
-      to={item.path}
-      className={classNames(cls.item, { [cls.collapsed]: collapsed })}
-    >
-      <item.Icon className={cls.icon} />
-      <span className={cls.link}>{t(item.text)}</span>
-    </AppLink>
+      <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={item.path}
+          className={classNames(cls.item, { [cls.collapsed]: collapsed })}
+      >
+        <item.Icon className={cls.icon} />
+        <span className={cls.link}>{t(item.text)}</span>
+      </AppLink>
   );
 });
