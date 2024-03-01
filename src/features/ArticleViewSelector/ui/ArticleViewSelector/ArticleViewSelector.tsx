f40fe61b82889477f5b1aafef15dd1dfe7200f11
luckyilia <ilia.dev@yahoo.com>
2024-03-01 23:39:02 +0300

    fix article view selector

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -1,11 +1,11 @@
-import { memo } from "react";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import ListIcon from "@/shared/assets/icons/list-24-24.svg";
-import TiledIcon from "@/shared/assets/icons/tiled-24-24.svg";
-import { Icon } from "@/shared/ui/Icon";
-import { Button, ButtonTheme } from "@/shared/ui/Button";
-import cls from "./ArticleViewSelector.module.scss";
-import { ArticleView } from "@/entities/Article";
+import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import ListIcon from '@/shared/assets/icons/list-24-24.svg';
+import TiledIcon from '@/shared/assets/icons/tiled-24-24.svg';
+import { Icon } from '@/shared/ui/Icon';
+import { Button, ButtonTheme } from '@/shared/ui/Button';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '@/entities/Article';
 
 interface ArticleViewSelectorProps {
   className?: string;
@@ -32,21 +32,23 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
   };
 
   return (
-    <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
-      {viewTypes.map((viewType) => (
-        <Button
-          key={viewType.view}
-          theme={ButtonTheme.CLEAR}
-          onClick={onClick(viewType.view)}
-        >
-          <Icon
-            Svg={viewType.icon}
-            className={classNames("", {
-              [cls.notSelected]: viewType.view !== view,
-            })}
-          />
-        </Button>
-      ))}
-    </div>
+      <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+        {viewTypes.map((viewType) => (
+            <Button
+                key={viewType.view}
+                theme={ButtonTheme.CLEAR}
+                onClick={onClick(viewType.view)}
+            >
+              <Icon
+                  width={24}
+                  height={24}
+                  Svg={viewType.icon}
+                  className={classNames('', {
+                    [cls.notSelected]: viewType.view !== view,
+                  })}
+              />
+            </Button>
+        ))}
+      </div>
   );
 });
